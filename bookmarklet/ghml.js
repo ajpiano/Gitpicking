0794c80c49d49269a731f660a7f98161686f492c
Jeremy Singer-Vine <jsvine@gmail.com>
2012-04-27 10:27:01 -0400

    Break up method-chaining, and break out regex.
    Should make adding new features and tweaking easier in the long run.

--- bookmarklet/ghml.js
+++ bookmarklet/ghml.js
@@ -1,21 +1,37 @@
-var wrappers, 
+var extension_pattern,
+	wrappers, 
 	tables, 
 	codes, 
 	styleElem;
 
-// Get all the wrapper diff wrapper elements for selected file extensions
-wrappers = $(".data.highlight").filter(function() {
-  return $(this).prev().data("path").match(/\.(md|txt|markdown|xml|rst|textile)$/);
-}).addClass("ghml-wrapper");
+// Declare the extensions of the files we want to apply this to.
+extension_pattern = /\.(md|txt|markdown|xml|rst|textile)$/;
 
-// Get all the tables inide and replace all non-breaking spaces with regular spaces
+// Get all the wrapper diff wrapper elements for selected file extensions.
+wrappers = $(".data.highlight").filter(function() {
+  return $(this).prev().data("path").match(extension_pattern);
+});
+
+// Add our special class to the matched elements.
+wrappers.addClass("ghml-wrapper");
+
+// Get all the tables inside
+tables = wrappers.children("table");
+
+// Add our special class.
+tables.addClass("ghml-table");
+
+// Replace all non-breaking spaces with regular spaces
 // Undoes GitHub's special sauce to prevent line-breaking
-tables = wrappers.children("table").addClass("ghml-table").html(function(i, html) {
+tables.html(function(i, html) {
   return html.replace(/&nbsp;/g, " ");
 });
 
-// Add a class to all the table cells that contain code/prose for styling
-codes = tables.find("td:nth-child(3)").addClass("ghml-code");
+// Find table cells that contain code/prose for styling.
+codes = tables.find("td:nth-child(3)");
+
+// Add a class to those table cells.
+codes.addClass("ghml-code");
 
 // Inject a stylesheet to control the appearance of spaces in code cells
 styleElem = $(["<style type=text/css>",

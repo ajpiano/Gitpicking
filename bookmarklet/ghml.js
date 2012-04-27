529c6ec56b419719e05c458cb8b8c428c3cd4da8
Jeremy Singer-Vine <jsvine@gmail.com>
2012-04-27 10:03:41 -0400

    Separate variable declaration and assignment.
    Assignments were a bit lengthy.

--- bookmarklet/ghml.js
+++ bookmarklet/ghml.js
@@ -1,15 +1,22 @@
-var
+var wrappers, 
+	tables, 
+	codes, 
+	styleElem;
+
 // Get all the wrapper diff wrapper elements for selected file extensions
 wrappers = $(".data.highlight").filter(function() {
   return $(this).prev().data("path").match(/\.(md|txt|markdown|xml|rst|textile)$/);
-}).addClass("ghml-wrapper"),
+}).addClass("ghml-wrapper");
+
 // Get all the tables inide and replace all non-breaking spaces with regular spaces
 // Undoes GitHub's special sauce to prevent line-breaking
 tables = wrappers.children("table").addClass("ghml-table").html(function(i, html) {
   return html.replace(/&nbsp;/g, " ");
-}),
+});
+
 // Add a class to all the table cells that contain code/prose for styling
-codes = tables.find("td:nth-child(3)").addClass("ghml-code"),
+codes = tables.find("td:nth-child(3)").addClass("ghml-code");
+
 // Inject a stylesheet to control the appearance of spaces in code cells
 styleElem = $(["<style type=text/css>",
   ".ghml-code pre{",
